f9cf815ffa2d65bc6568f2dd108847d81d4beab1
hatimabualrub <64695576+hatimabualrub@users.noreply.github.com>
2020-09-01 13:24:05 +0300

    Updates

---
+++ precache-manifest.0a6ef1121d4cecae9247cc386418ccec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525d142efc9a4b126e9d69929450ee61",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "c5208e857fe0b40487ad",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d7ef2772a552f75b7171",
+    "url": "/facerecognition/static/css/main.6d5d0540.chunk.css"
+  },
+  {
+    "revision": "c5208e857fe0b40487ad",
+    "url": "/facerecognition/static/js/2.091c3209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.091c3209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ef2772a552f75b7171",
+    "url": "/facerecognition/static/js/main.9b54dcbf.chunk.js"
+  },
+  {
+    "revision": "fa6a11a9cd7eebd80802",
+    "url": "/facerecognition/static/js/runtime-main.1ad3eaac.js"
+  }
+]);
